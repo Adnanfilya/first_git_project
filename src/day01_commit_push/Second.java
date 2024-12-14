c2e7522f5d54b96c98b9619ceb272f5f65be6106
Afilya <adnanfilya24@gmail.com>
2024-12-15 01:04:49 +0300

    updated econd.java

--- src/day01_commit_push/Second.java
+++ src/day01_commit_push/Second.java
@@ -3,5 +3,6 @@ package day01_commit_push;
 public class Second {
     public static void main(String[] args) {
         System.out.println("Hello from second");
+        System.out.println("More changes here");
     }
 }
