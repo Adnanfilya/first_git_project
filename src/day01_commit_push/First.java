bba524cb2e15f0dbf690bf385d983803cd45a630
Afilya <adnanfilya24@gmail.com>
2024-12-15 00:59:50 +0300

    update first.java and added second.java

--- src/day01_commit_push/First.java
+++ src/day01_commit_push/First.java
@@ -3,5 +3,6 @@ package day01_commit_push;
 public class First {
     public static void main(String[] args) {
         System.out.println("hello from first class!");
+        System.out.println("New Line in first class");
     }
 }
