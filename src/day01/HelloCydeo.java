ef5cb79522d76fb2674d5c808d0dc41e735d7551
Afilya <adnanfilya24@gmail.com>
2024-12-22 12:33:09 +0300

    In cydeo i changed sout.

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -2,9 +2,7 @@ package day01;
 
 public class HelloCydeo {
     public static void main(String[] args) {
-
         System.out.println("Hello Cydeo");
-        System.out.println("Hello Cydeo2");
-        System.out.println("Hello Cydeo3");
+        System.out.println("deneme1");
     }
 }
