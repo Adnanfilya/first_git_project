1af7fa06198775cbcdf506a72571175721b5ef3a
Adnanfilya <adnanfilya24@gmail.com>
2024-12-22 12:35:33 +0300

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,5 +4,7 @@ public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
         System.out.println("deneme1");
+        System.out.println("deneme2");
+        
     }
 }
